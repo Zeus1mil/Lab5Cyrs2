79e9a0bc7bb0608b69f4656399c0e0c98f0dec97
k1rn <166209066+Zeus1mil@users.noreply.github.com>
2025-04-23 20:33:43 +0300

    Add files via upload

---
+++ Work5Hard/src/ChocolateFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace UserP4\Work5hard\Chocolate;
+
+use UserP4\Work5hard\Chocolate\Sweets;
+
+class ChocolateFactory
+{
+    public function orderSnack(string $type): void
+    {
+        $snack = $this->createSnack(type: $type);
+        $snack->prepare();
+        $snack->cut();
+    }
+    public function createSnack(string $type): Sweets
+    {
+        return new Sweets(name: "", chocolate: "", toppings: [""]);
+    }
+}
